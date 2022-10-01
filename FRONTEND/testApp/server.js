963dbc96e873490dba310c36ca0e9c3ff3252591
Eva Diaw <ediaw@cfai-formation.fr>
2022-10-01 16:10:51 +0200

    update server.js

--- FRONTEND/testApp/server.js
+++ FRONTEND/testApp/server.js
@@ -4,10 +4,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/testApp"));
+app.use(express.static(__dirname + "/dist/test-app"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/testApp/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/test-app/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
